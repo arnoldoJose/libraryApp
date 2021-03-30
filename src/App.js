27640167c7e5b52a78505b4628223044ad36ae42
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-30 17:35:02 -0600

    change history

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { CRMAuthContext } from './Context/AuthProvider'
 import { Provider } from './Context/Provider';
 //categories'
 import Novelas from './Components/Categories/Novelas/Novelas';
+import History from './Components/History/History';
 import Poemas from './Components/Categories/Poemas/Poemas';
 import Documentaries from "./Components/Categories/Documentaries/Documentaries";
 import Otros from "./Components/Categories/Otros/Otros";
@@ -35,31 +36,34 @@ function App() {
   return (
     <>
       <Router>
-          <Provider>
-            <Switch>
-              <Route exact path="/libraryApp/" component={Main} />
-              <Route exact path="/book/:id" component={Book} />
-              <Route exact path="/books" component={Books} />
-              <Route exact path="/novela" component={Novelas} />
-              <Route exact path="/poema" component={Poemas} />
-              <Route exact path="/documentales" component={Documentaries} />
-              <Route exact path="/otros" component={Otros} />
-              <Route exact path="/authors" component={Authors} />
-              <Route exact path="/news" component={News} />
-              <Route exact path="/admin/login" component={Login} />
-              
-              {(auth.auth || localStorage.getItem("token")) ? (
-                <Route exact path="/admin/count" component={CaseAdmin} />
-              ) : (<Redirect to="/admin/login" />)}
+        <Provider>
+          <Switch>
+            <Route exact path="/libraryApp/" component={Main} />
+            <Route exact path="/history/" component={History} />
+            <Route exact path="/book/:id" component={Book} />
+            <Route exact path="/books" component={Books} />
+            <Route exact path="/novela" component={Novelas} />
+            <Route exact path="/poema" component={Poemas} />
+            <Route exact path="/documentales" component={Documentaries} />
+            <Route exact path="/otros" component={Otros} />
+            <Route exact path="/authors" component={Authors} />
+            <Route exact path="/news" component={News} />
+            <Route exact path="/admin/login" component={Login} />
 
-              <Route exact path="/admin/loans" component={Prestamos} />
-              <Route exact path="/admin/books" component={EspecificBook} />
-              <Route exact path="/admin/add/books" component={FormAddBook} />
-              <Route exact path="/admin/reservations" component={Reservations}/>
-              <Route exact path="/admin/returns" component={Returns} />
-              <Route exact path="/admin/loan/:id" component={FormLoans} />
-            </Switch>
-          </Provider>
+            {auth.auth || localStorage.getItem("token") ? (
+              <Route exact path="/admin/count" component={CaseAdmin} />
+            ) : (
+              <Redirect to="/admin/login" />
+            )}
+
+            <Route exact path="/admin/loans" component={Prestamos} />
+            <Route exact path="/admin/books" component={EspecificBook} />
+            <Route exact path="/admin/add/books" component={FormAddBook} />
+            <Route exact path="/admin/reservations" component={Reservations} />
+            <Route exact path="/admin/returns" component={Returns} />
+            <Route exact path="/admin/loan/:id" component={FormLoans} />
+          </Switch>
+        </Provider>
       </Router>
     </>
   );}
